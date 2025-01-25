53d2b1c56c0218b217781b8c090820677e994adf
Helgarold <mipakk95@gmail.com>
2025-01-25 14:58:49 +0300

    Update calc.java

--- Calculate/src/main/java/org/example/calc.java
+++ Calculate/src/main/java/org/example/calc.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import java.util.Scanner;
-
 class Main {
     public static void main(String[] args) {
         System.out.println("Введите выражение:");
@@ -16,30 +14,45 @@ class Main {
     }
 
     public static String calc(String input) throws Exception {
-        if (!input.matches("\\d+ [\\+\\-*/] \\d+")) {
+        if (!input.matches("\\d+ [\\+\\-*/] \\d+ [\\+\\-*/] \\d+")) {
             throw new Exception("Неправильное выражение");
         }
 
         String[] parts = input.split(" ");
         int num1 = Integer.parseInt(parts[0]);
         int num2 = Integer.parseInt(parts[2]);
+        int num3 = Integer.parseInt(parts[4]);
+        char op1 = parts[3].charAt(0);
         char op = parts[1].charAt(0);
 
-        if (num1 < 1 || num1 > 10 || num2 < 1 || num2 > 10) {
+        if (num1 < 1 || num1 > 10 || num2 < 1 || num2 > 10 || num3 < 1 || num3 > 10) {
             throw new Exception("Числа вне диапазона 1-10");
         }
 
+        // Реализация очереди выполнения операций
+        if (op == '*' || op == '/') {
+            num1 = getSomeResult(num1, num2, op);
+            return String.valueOf(getSomeResult(num1, num3, op1));
+        } else if (op1 == '*' || op1 == '/') {
+            num2 = getSomeResult(num2, num3, op1);
+            return String.valueOf(getSomeResult(num1, num2, op));
+        } else {
+            return String.valueOf(getSomeResult(getSomeResult(num1, num2, op), num3, op1));
+        }
+    }
+
+    public static int getSomeResult(int num1, int num2, char op) throws Exception {
         switch (op) {
             case '+':
-                return String.valueOf("Результат операции: " + (num1 + num2));
+                return num1 + num2;
             case '-':
-                return String.valueOf("Результат операции: " + (num1 - num2));
+                return num1 - num2;
             case '*':
-                return String.valueOf("Результат операции: " + (num1 * num2));
+                return num1 * num2;
             case '/':
-                return String.valueOf("Результат операции: " + (num1 / num2));
+                return num1 / num2;
             default:
-                throw new Exception("Неизвестная операция");
+                throw new Exception("Неизвестная операция: " + op);
         }
     }
 }
